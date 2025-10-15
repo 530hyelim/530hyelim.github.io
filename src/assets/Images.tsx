dabda451cdef4e51c07e4696ec6f67a6e5a5fe24
530hyelim <530hyelim@gmail.com>
2025-10-16 08:08:56 +0900

    no message

---
+++ src/assets/Images.tsx
@@ -0,0 +1,49 @@
+import main from './ypjp/ypjp.png'
+import mangae from './ypjp/10000.png'
+import smn from './ypjp/smn.png'
+import calendar from './ypjp/calendar.png'
+import structure from './ypjp/structure.png'
+import ucd from './ypjp/ucd.png'
+import erd from './ypjp/erd.png'
+
+export const ypjp = {
+    main,
+    mangae,
+    smn,
+    calendar,
+    structure,
+    ucd,
+    erd,
+}
+
+import nlmain from './newLearn/main.png'
+import slack from './newLearn/slack.png'
+import github from './newLearn/github.png'
+import discord from './newLearn/discord.png'
+import nlcalendar from './newLearn/nlcalendar.png'
+import nlstructure from './newLearn/nlstructure.png'
+import nlucd from './newLearn/nlucd.png'
+import nlerd from './newLearn/nlerd.png'
+
+export const newlearn = {
+    main: nlmain,
+    slack,
+    github,
+    discord,
+    calendar: nlcalendar,
+    structure: nlstructure,
+    ucd: nlucd,
+    erd: nlerd,
+}
+
+import langsmith from './icons/langsmith-color.png'
+import lombok from './icons/lombok.png'
+import jsp from './icons/28968.png'
+import mybatis from './icons/마이바티스_로고.png'
+
+export const icon = {
+    langsmith,
+    lombok,
+    jsp,
+    mybatis,
+}
